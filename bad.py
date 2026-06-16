fab14bb4d3525af42153fa2e5153635eef091ac3
Fulu1014 <709287419@qq.com>
2026-06-16 21:12:29 +0800

    Update bad.py

--- bad.py
+++ bad.py
@@ -6,6 +6,9 @@ def process_user_input():
     user_input = input("请输入一个数学表达式: ")
     # 安全漏洞：直接使用 eval 执行用户输入的字符串，可能导致任意代码执行 (RCE)
     result = eval(user_input)
+
+    user_input2 = input("请输入一个数学表达式：")
+    result2 = eval(user_input2) 
     print(f"计算结果是: {result}")
 
 if __name__ == "__main__":
