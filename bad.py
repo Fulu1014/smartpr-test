35afa98b8f99dc5a35c01fea6d945348ad595d78
Fulu1014 <709287419@qq.com>
2026-06-16 21:31:49 +0800

    Update bad.py

--- bad.py
+++ bad.py
@@ -1,21 +1,9 @@
 def calculate_average(total, count):
-    # 逻辑漏洞：没有检查 count 是否为 0
-    return total / count
-
-def calculate_average2(total, count):
-    # 逻辑漏洞：没有检查 count 是否为 0
     return total / count
 
 def process_user_input():
     user_input = input("请输入一个数学表达式: ")
-    # 安全漏洞：直接使用 eval 执行用户输入的字符串，可能导致任意代码执行 (RCE)
     result = eval(user_input)
-
-    user_input2 = input("请输入一个数学表达式：")
-    result2 = eval(user_input2) 
-
-    user_input3 = input("请输入一个数学表达式：")
-    result3 = eval(user_input2) 
     print(f"计算结果是: {result}")
 
 if __name__ == "__main__":
